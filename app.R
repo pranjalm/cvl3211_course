d7d6ab77848d0d8d6e9776d5c005878e3ecb1c80
pranjal mandhaniya <pranjalmandhaniya@gmail.com>
2016-09-30 13:34:15 +0530

    second

--- app.R
+++ app.R
@@ -15,11 +15,14 @@ ui <- shinyUI(fluidPage(
   # Application title
   titlePanel("CVL3211 Student Records"),
   
-  # Sidebar with a slider input for number of bins Attandence_percentage , Quiz_marks_12
+  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
       textInput("Roll_Number","Roll Number",NULL) ,
-      textInput("Name","Name",NULL)  
+      textInput("Name","Name",NULL),
+      p("In case of Quiz- 'x' means absent. Quiz 1 marks are out of 12."),
+      p("In case of Class- 'x' means Unregistered so those days won't count for attendance percent."),
+      p("link for the overall attendence sheet is https://github.com/pranjalm/iter_dir/blob/master/section_c.csv")
     ),
     
     # Show a plot of the generated distribution
@@ -38,8 +41,9 @@ server <- shinyServer(function(input, output) {
   output$dispres <- renderTable({
     
     # generate bins based on input$bins from ui.R
-    #setwd("~/Desktop/ITER/CVL3211/attandence/section_c")
-    data <- read.csv("section_c.csv")
+    library(RCurl)
+    x <- getURL("https://raw.githubusercontent.com/pranjalm/iter_dir/master/section_c.csv")
+    data <- read.csv(text = x)
     p<-0
     a<-0
     b <- c()
@@ -53,8 +57,8 @@ server <- shinyServer(function(input, output) {
       a<-0
       p<-0
     }
-    d <- data.frame(data$reg_no,data$name,b,data$Q1)
-    colnames(d)<- c("Roll_Number","Name","Attandence_percentage","Quiz_marks_1")
+    d <- data.frame(data$reg_no,data$name,b,data$Q1,data$Q2)
+    colnames(d)<- c("Roll_Number","Name","Attendance_percentage","Quiz_1_marks","Quiz_2_marks")
     # find title in data$Title
     query1 <- grepl(input$Roll_Number , d$Roll_Number,ignore.case = TRUE)
     query2 <- grepl(input$Name , d$Name,ignore.case = TRUE)
